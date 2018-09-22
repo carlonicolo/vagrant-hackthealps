9af19756d27a07bf2dd9e03967f82a06a7d00201
bonte <andrea.bonte92@hotmail.it>
2018-09-22 20:18:09 +0200

    add pages

--- projects/app/__init__.py
+++ projects/app/__init__.py
@@ -6,6 +6,24 @@ import random
 app = Flask(__name__)
 fake = Faker()
 
+def _generateData():
+    racelist = ['Bruna', 'Bianca', 'Frisona']
+    birthpalcelist = ['Bolzano', 'Merano', 'Bressanone']
+    animals = []
+
+    for i in range(0, 10):
+        elem = {}
+        elem["ref"] = i
+        elem["code"] = fake.ean8()
+        elem["birthdate"] = fake.date_this_decade(before_today=True, after_today=False)
+        elem["race"] = random.choice(racelist)
+        elem["birthplace"] = random.choice(birthpalcelist)
+        animals.append(elem)
+    return animals
+
+
+data = _generateData()
+
 
 @app.route('/hello')
 def hello_world():
@@ -24,19 +42,19 @@ def map():
 
 @app.route('/animals')
 def animals():
-    racelist = ['Bruna', 'Bianca', 'Frisona']
-    birthpalcelist = ['Bolzano', 'Merano', 'Bressanone']
-    animals = []
+    print(data)
+    return render_template("animals.html", page='animals', data = data)
 
-    for i in range(0,10):
-        elem = {}
-        elem["ref"] = i
-        elem["code"] = fake.ean8()
-        elem["birthdate"] = fake.date_this_decade(before_today=True, after_today=False)
-        elem["race"] = random.choice(racelist)
-        elem["birthplace"]=random.choice(birthpalcelist)
-        animals.append(elem)
-    return render_template("animals.html", page='animals', data = animals)
+
+@app.route('/animal/<id>')
+def animal(id):
+    elem={}
+    for e in data:
+        if e["ref"] == int(id):
+            elem = e
+    return render_template("animal.html", id=id, elem= elem)
+
+
 
 
 if __name__ == '__main__':
